a019fb98fcc81f2678a76fe363cc73e132f9bc6e
Carlos O'Ryan <coryan@google.com>
2026-07-27 18:20:35 -0400

    chore: update librarian circa 2026-07-27 (#48)
    In this version, librarian generates correct cross-reference links for
    enums. This fixes a number of documentation warnings for
    `google-cloud-wkt` and many generated libraries.

--- Sources/GoogleCloudBigqueryConnectionV1/CloudSqlProperties.swift
+++ Sources/GoogleCloudBigqueryConnectionV1/CloudSqlProperties.swift
@@ -110,7 +110,7 @@ public struct CloudSqlProperties: Codable, Equatable, GoogleCloudWkt._AnyPackabl
 
     /// Initialize from a string value.
     ///
-    /// If the value is unknown, this initializes to ``.unknownStringValue(_:)``.
+    /// If the value is unknown, this initializes to [`unknownStringValue`](doc:DatabaseType/unknownStringValue(_:)).
     public init(stringValue: Swift.String) {
       switch stringValue {
       case "DATABASE_TYPE_UNSPECIFIED": self = .unspecified
@@ -122,7 +122,7 @@ public struct CloudSqlProperties: Codable, Equatable, GoogleCloudWkt._AnyPackabl
 
     /// Initialize from an integer value.
     ///
-    /// If the value is unknown, this initializes to ``.unknownIntValue(_:)``.
+    /// If the value is unknown, this initializes to [`unknownIntValue`](doc:DatabaseType/unknownIntValue(_:)).
     public init(intValue: Int) {
       switch intValue {
       case 0: self = .unspecified
